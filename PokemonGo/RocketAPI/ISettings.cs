fb4884ed8c931a086dba4cf203281ebea16cfd51
Sen66 <Sebastianr266@gmail.com>
2016-07-23 00:37:15 +0200

    first commit

---
+++ PokemonGo/RocketAPI/ISettings.cs
@@ -0,0 +1,30 @@
+#region
+
+using PokemonGo.RocketAPI.Enums;
+using System.Collections.Generic;
+
+#endregion
+
+namespace PokemonGo.RocketAPI
+{
+    public interface ISettings
+    {
+        AuthType AuthType { get; }
+        double DefaultLatitude { get; set; }
+        double DefaultLongitude { get; set; }
+        string LevelOutput { get; }
+        int LevelTimeInterval { get; }
+        string GoogleRefreshToken { get; set; }
+        string PtcPassword { get; }
+        string PtcUsername { get; }
+        bool EvolveAllGivenPokemons { get; }
+        string TransferType { get; }
+        int TransferCPThreshold { get; }
+        bool Recycler { get; }
+        ICollection<KeyValuePair<AllEnum.ItemId, int>> ItemRecycleFilter { get; }
+        int RecycleItemsInterval { get; }
+        string Language { get; }
+        string RazzBerryMode { get; }
+        double RazzBerrySetting { get; }
+    }
+}
